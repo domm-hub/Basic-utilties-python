cc6e52a6f8a9b804ced3f925c8e9536bb502a9f6
Namemamamx <116433946+domm-hub@users.noreply.github.com>
2024-02-16 13:33:45 +0100

    Update main.py

--- QRCodeGenerator/main.py
+++ QRCodeGenerator/main.py
@@ -6,7 +6,14 @@ import customtkinter as ctk
 window = ctk.CTk()
 window.title("QRCode Generator")
 window.geometry("600x600")
-window.minsize(600, 600)
+window.resizable(False, False)
+
+frame = ctk.CTkScrollableFrame(window, 600, 600)
+frame.pack()
+
+def get_window_height():
+    height = frame.winfo_height()  # Get the height of the frame instead of the window
+    print("Current window height:", height)
 
 def createqr():
     data = URL_TextBox.get() 
@@ -17,23 +24,23 @@ def createqr():
 
 bigfont = ctk.CTkFont("Calibri", 30)
 
-label = ctk.CTkLabel(window, text="QRCode Generator", font=bigfont)
+label = ctk.CTkLabel(frame, text="QRCode Generator", font=bigfont)
 label.pack()
 
-label = ctk.CTkLabel(window, text="URL or Data:")
+label = ctk.CTkLabel(frame, text="URL or Data:")
 label.pack(pady=10)
 
-URL_TextBox = ctk.CTkEntry(window)
+URL_TextBox = ctk.CTkEntry(frame)
 URL_TextBox.pack()
 
-Photo = ctk.CTkLabel(window, text="")
+Photo = ctk.CTkLabel(frame, text="")
 Photo.pack(pady=10)
 
-create_btn = ctk.CTkButton(window, text="Create!", command=createqr)
+create_btn = ctk.CTkButton(frame, text="Create!", command=createqr)
 create_btn.pack(pady=40)
 
 # "Save" button
-save_btn = ctk.CTkButton(window, text="Save", command=lambda: save_qr())
+save_btn = ctk.CTkButton(frame, text="Save", command=lambda: save_qr())
 save_btn.pack()
 
 def save_qr():
@@ -45,5 +52,7 @@ def save_qr():
     else:
         from tkinter import messagebox
         messagebox.showinfo("Info", "Please Make A QR Code")
+
+
 
 window.mainloop()
